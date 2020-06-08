ae16d7b46687ff116e4e7a3dd887e8fb97ef79b4
Magdalena Lisicka <magdalisicka@yahoo.co.uk>
2020-06-08 22:34:22 +0100

    js import export test

---
+++ hello.js
@@ -0,0 +1,5 @@
+export const hello = (name) => {
+    return `Czesc ${name}`;
+}
+
+export const korsi = 'Maciek';
---
+++ main.js
@@ -0,0 +1,8 @@
+import {hello, korsi} from './hello.js';
+import maciek from './maciek.js';
+
+console.log(hello('Maciek'));
+
+console.log(korsi);
+
+console.log(maciek);
